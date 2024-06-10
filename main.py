aa82c9bb748ea385a7f8d2b2af462ccacb93f9d7
Dmitriy Kucheriaviy <dmitask@gmail.com>
2024-06-11 00:16:09 +0300

    update situation with open deposit

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def format_date(date_str):
     date_obj = datetime.fromisoformat(date_str)
     return date_obj.strftime("%d.%m.%Y")
 
+
 def format_operation(operation):
     date = format_date(operation["date"])
     description = operation["description"]
@@ -23,12 +24,14 @@ def format_operation(operation):
     to = operation["to"]
 
     # Определяем тип отправителя (карта или счет)
-    if from_.startswith("Visa") or from_.startswith("Maestro"):
+    if from_ and (from_.startswith("Visa") or from_.startswith("Maestro")):
         from_name = " ".join(from_.split()[:-1])  # Получаем название карты без номера
         from_masked = mask_card_number(from_.split()[-1])
+        from_formatted = f"{from_name} {from_masked}"
+    elif from_:
+        from_formatted = mask_account_number(from_)
     else:
-        from_name = "Счет"
-        from_masked = mask_account_number(from_)
+        from_formatted = ""
 
     # Определяем тип получателя (счет)
     to_name = "Счет"
@@ -37,8 +40,13 @@ def format_operation(operation):
     amount = operation["operationAmount"]["amount"]
     currency_name = operation["operationAmount"]["currency"]["name"]
 
-    formatted_operation = f"{date} {description}\n{from_name} {from_masked} -> {to_name} {to_masked}\n{amount} {currency_name}\n"
+    if from_formatted:
+        formatted_operation = f"{date} {description}\n{from_formatted} -> {to_name} {to_masked}\n{amount} {currency_name}\n"
+    else:
+        formatted_operation = f"{date} {description}\n{to_name} {to_masked}\n{amount} {currency_name}\n"
+
     return formatted_operation
+
 
 def get_last_operations(file_path, n=5):
     with open(file_path, "r", encoding="utf-8") as file:
